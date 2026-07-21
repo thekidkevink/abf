596ea6d59e0be7a01919213afc7e0368ab05f5cf
Mbitjita Ndjambi Kamapunga <140601916+thekidkevink@users.noreply.github.com>
2026-07-21 03:03:11 +0200

    Hurrah

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  images: { remotePatterns: [{ protocol: "https", hostname: "images.unsplash.com" }] },
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "images.unsplash.com" }],
+  },
   outputFileTracingRoot: path.join(__dirname),
+  output: "export",
 };
 export default nextConfig;
